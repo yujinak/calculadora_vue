15e7c1f7a212c634ba59ad3de73bacdb5cde5327
Yuji <g.yujinak@gmail.com>
2023-11-20 10:33:28 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { reactive } from "vue";
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="">Calculadora VueJS</h1>
+    <form action=""></form>
+  </div>
+</template>
+
+<style scoped></style>
